bb0a81eee92546584867f3f94a6eda53c972325d
ghashy <obsidian.musicwork@gmail.com>
2023-12-15 06:21:31 +0300

    add confirmation email sending, improve perfomance

--- src/auth/signup.rs
+++ src/auth/signup.rs
@@ -1,15 +1,20 @@
 use anyhow::Context;
 use argon2::password_hash::SaltString;
 use argon2::PasswordHasher;
+use askama::Template;
 use axum::extract::State;
 use axum::Form;
 use http::StatusCode;
-use secrecy::ExposeSecret;
+use secrecy::{ExposeSecret, Secret};
 
-use crate::domain::user_email::UserEmail;
 use crate::domain::user_name::UserName;
 use crate::domain::user_password::UserPassword;
+use crate::email_client::EmailClient;
 use crate::startup::AppState;
+use crate::telemetry::spawn_blocking_with_tracing;
+use crate::{
+    domain::user_email::UserEmail, validation::signup_token::SignupToken,
+};
 
 use super::{AuthError, UserSignupData};
 
@@ -22,40 +27,97 @@ pub async fn signup(
         email,
     }): Form<UserSignupData>,
 ) -> Result<StatusCode, AuthError> {
-    let username = UserName::parse(&username)
-        .map_err(AuthError::InvalidCredentialsError)?;
-    let email =
-        UserEmail::parse(&email).map_err(AuthError::InvalidCredentialsError)?;
+    let username =
+        UserName::parse(&username).map_err(AuthError::SignupFailed)?;
+    let email = UserEmail::parse(&email).map_err(AuthError::SignupFailed)?;
     let password =
         UserPassword::parse(&password, &[username.as_ref(), email.as_ref()])
-            .map_err(AuthError::InvalidCredentialsError)?;
+            .map_err(AuthError::SignupFailed)?;
 
-    let salt = SaltString::generate(&mut rand::thread_rng());
+    let password_hash = spawn_blocking_with_tracing(move || {
+        hash_password(password.as_ref(), app_state.argon2_obj)
+    })
+    .await
+    .context("Failed to join thread")
+    .map_err(AuthError::InternalError)??;
 
-    // TODO: benchmark this function. Should we do it in a dedicated thread?
-    let password_hash = app_state
-        .argon2_obj
-        .hash_password(password.as_ref().expose_secret().as_bytes(), &salt)
-        .context("Failed to hash password")?
-        .to_string();
+    let client = app_state
+        .pool
+        .get()
+        .await
+        .context("Failed to get connection from pool")
+        .map_err(AuthError::InternalError)?;
 
-    let client = app_state.pool.get().await.map_err(|e| {
-        tracing::error!("{e}");
-        AuthError::InternalError
-    })?;
+    let validation_token = SignupToken::generate();
 
     let rows_count = client
         .execute(
-            "INSERT INTO users (username, email, password_hash)
-                    VALUES ($1, $2, $3)",
-            &[&username.as_ref(), &email.as_ref(), &password_hash],
+            "INSERT INTO user_candidates (username, email, password_hash, validation_token)
+                    VALUES ($1, $2, $3, $4)",
+            &[&username.as_ref(), &email.as_ref(), &password_hash, &validation_token.as_ref()],
         )
         .await
-        .expect("Failed to create test users");
+        .context("Failed to insert user data to db")?;
 
     if rows_count != 1 {
-        return Err(AuthError::UserCreationFailed);
+        return Err(AuthError::SignupFailed(anyhow::anyhow!(
+            "Inserted {rows_count} rows to db"
+        )));
     }
 
+    send_confirmation_email(
+        &app_state.email_client,
+        &username,
+        &email,
+        &app_state.base_url,
+        &validation_token,
+    )
+    .await
+    .context("Failed to send confirmation email")?;
+
     Ok(StatusCode::CREATED)
+}
+
+#[tracing::instrument(name = "Performing hashing of password", skip_all)]
+fn hash_password(
+    password: &Secret<String>,
+    argon2: argon2::Argon2,
+) -> Result<String, AuthError> {
+    let salt = SaltString::generate(&mut rand::thread_rng());
+    Ok(argon2
+        .hash_password(password.expose_secret().as_bytes(), &salt)
+        .context("Failed to hash password")
+        .map_err(AuthError::SignupFailed)?
+        .to_string())
+}
+
+#[tracing::instrument(
+    name = "Send a confirmation email to a new user",
+    skip_all
+)]
+pub async fn send_confirmation_email(
+    email_client: &EmailClient,
+    username: &UserName,
+    user_email: &UserEmail,
+    base_url: &str,
+    signup_token: &SignupToken,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link = format!(
+        "{}/subscriptions/confirm?subscription_token={}",
+        base_url,
+        signup_token.as_ref()
+    );
+    let html_body = crate::html_template_gen::VerifyEmailTemplate::new(
+        username.as_ref(),
+        &confirmation_link,
+    )
+    .render()
+    .unwrap();
+    let plain_body = format!(
+        "Welcome to mustore!\nVisit {} to confirm your account.",
+        confirmation_link
+    );
+    email_client
+        .send_email(&user_email, "Welcome!", &html_body, &plain_body)
+        .await
 }
