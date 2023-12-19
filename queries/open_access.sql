fe31e76ece570f53e29cc4515a0cf05b92d9002e
ghashy <obsidian.musicwork@gmail.com>
2023-12-19 22:14:37 +0300

    add queries for routes with open access

---
+++ queries/open_access.sql
@@ -0,0 +1,21 @@
+--! get_stats
+(
+    SELECT 'songs' AS table_name, COUNT(*) as count
+    FROM songs
+)
+UNION ALL
+(
+    SELECT 'beats' AS table_name, COUNT(*) as count
+    FROM beats
+)
+UNION ALL
+(
+    SELECT 'covers' AS table_name, COUNT(*) as count
+    FROM covers
+)
+UNION ALL
+(
+    SELECT 'lyrics' AS table_name, COUNT(*) as count
+    FROM lyrics
+);
+
