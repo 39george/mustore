0d23de3abd08f6423ffc2ff0802d275f6dcc2979
ghashy <obsidian.musicwork@gmail.com>
2023-12-22 16:26:03 +0300

    update api

--- src/routes/open/mod.rs
+++ src/routes/open/mod.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use anyhow::Context;
+use axum::extract::Path;
 use axum::extract::State;
 use axum::response::IntoResponse;
 use axum::response::Response;
@@ -13,10 +14,9 @@ use serde::Serialize;
 
 // ───── Current Crate Imports ────────────────────────────────────────────── //
 
-use crate::cornucopia::queries::open_access::get_genres_list;
-use crate::cornucopia::queries::open_access::get_stats;
-use crate::cornucopia::queries::open_access::get_tags_list;
-use crate::cornucopia::queries::open_access::GetStats;
+use crate::cornucopia::queries::open_access;
+use crate::cornucopia::queries::open_access::GetSongs;
+use crate::domain::music_parameters::*;
 use crate::error_chain_fmt;
 use crate::startup::AppState;
 
@@ -28,6 +28,8 @@ pub enum ResponseError {
     UnexpectedError(#[from] anyhow::Error),
     #[error("Internal error")]
     InternalError(#[source] anyhow::Error),
+    #[error("Bad request")]
+    BadRequest(#[source] anyhow::Error),
 }
 
 impl std::fmt::Debug for ResponseError {
@@ -46,6 +48,9 @@ impl IntoResponse for ResponseError {
             ResponseError::InternalError(_) => {
                 StatusCode::INTERNAL_SERVER_ERROR.into_response()
             }
+            ResponseError::BadRequest(_) => {
+                StatusCode::BAD_REQUEST.into_response()
+            }
         }
     }
 }
@@ -58,9 +63,11 @@ struct Stats {
     covers: u32,
 }
 
-impl TryFrom<Vec<GetStats>> for Stats {
+impl TryFrom<Vec<open_access::GetStats>> for Stats {
     type Error = anyhow::Error;
-    fn try_from(value: Vec<GetStats>) -> Result<Self, Self::Error> {
+    fn try_from(
+        value: Vec<open_access::GetStats>,
+    ) -> Result<Self, Self::Error> {
         let hash_map: HashMap<String, u32> = value
             .into_iter()
             .map(|v| (v.table_name, v.count as u32))
@@ -82,13 +89,24 @@ impl TryFrom<Vec<GetStats>> for Stats {
     }
 }
 
+#[derive(Deserialize, Debug)]
+struct GetSongsListQuery {
+    sex: Option<Sex>,
+    tempo: Option<Vec<i16>>,
+    key: Option<Vec<MusicKey>>,
+    genres: Option<Vec<String>>,
+    vibes: Option<Vec<String>>,
+    sort_by: SortBy,
+    amount: i64,
+}
+
 // ───── Body ─────────────────────────────────────────────────────────────── //
 
 pub fn open_router() -> Router<AppState> {
     Router::new()
         .route("/stats", routing::get(stats))
-        .route("/genres", routing::get(genres))
-        .route("/tags", routing::get(tags))
+        .route("/:what", routing::get(get_values_list))
+        .route("/songs", routing::get(get_songs))
 }
 
 #[tracing::instrument(name = "Get products counts (stats)", skip_all)]
@@ -100,7 +118,7 @@ async fn stats(
         .get()
         .await
         .context("Failed to get pool from pg")?;
-    let stats: Stats = get_stats()
+    let stats: Stats = open_access::get_stats()
         .bind(&pg_pool)
         .all()
         .await
@@ -110,9 +128,10 @@ async fn stats(
     Ok(Json::from(stats))
 }
 
-/// We should return json list with genres
-#[tracing::instrument(name = "Get genres list", skip_all)]
-async fn genres(
+/// We should return json list
+#[tracing::instrument(name = "Get values list", skip(app_state))]
+async fn get_values_list(
+    Path(path): Path<String>,
     State(app_state): State<AppState>,
 ) -> Result<Json<Vec<String>>, ResponseError> {
     let pg_pool = app_state
@@ -121,30 +140,68 @@ async fn genres(
         .await
         .context("Failed to get pool from pg")?;
 
-    let genres = get_genres_list()
-        .bind(&pg_pool)
-        .all()
-        .await
-        .context("Failed to get genres list from pg")?;
-
-    Ok(Json(genres))
+    match path.as_str() {
+        "genres" => Ok(Json(
+            open_access::get_genres_list()
+                .bind(&pg_pool)
+                .all()
+                .await
+                .context("Failed to get genres list from pg")?,
+        )),
+        "tags" => Ok(Json(
+            open_access::get_tags_list()
+                .bind(&pg_pool)
+                .all()
+                .await
+                .context("Failed to get genres list from pg")?,
+        )),
+        _ => Err(ResponseError::BadRequest(anyhow::anyhow!(
+            "Can't send values of {path}!"
+        ))),
+    }
 }
 
-#[tracing::instrument(name = "Get tags list", skip_all)]
-async fn tags(
+#[tracing::instrument(name = "Get songs query", skip(app_state))]
+async fn get_songs(
     State(app_state): State<AppState>,
-) -> Result<Json<Vec<String>>, ResponseError> {
+    Json(params): Json<GetSongsListQuery>,
+) -> Result<Json<Vec<GetSongs>>, ResponseError> {
+    if params.amount > 50 || params.amount < 1 {
+        return Err(ResponseError::BadRequest(anyhow::anyhow!(
+            "Amount of tracks should be between 1 and 50, requested: {}",
+            params.amount
+        )));
+    }
+    if params
+        .tempo
+        .as_ref()
+        .is_some_and(|v| v.len() != 2 || v[0] < 40 || v[1] > 320)
+    {
+        return Err(ResponseError::BadRequest(anyhow::anyhow!(
+            "Tempo should be array with 2 values, min is 40 and max is 320!"
+        )));
+    }
+
     let pg_pool = app_state
         .pg_pool
         .get()
         .await
         .context("Failed to get pool from pg")?;
-
-    let tags = get_tags_list()
-        .bind(&pg_pool)
+    let songs = open_access::get_songs()
+        .bind(
+            &pg_pool,
+            &params.sex.map(|s| s.to_string()),
+            &params.tempo,
+            &params
+                .key
+                .map(|v| v.into_iter().map(|el| el.into()).collect::<Vec<_>>()),
+            &params.genres,
+            &params.vibes,
+            &params.sort_by.to_string(),
+            &params.amount,
+        )
         .all()
         .await
-        .context("Failed to get genres list from pg")?;
-
-    Ok(Json(tags))
+        .context("Failed to fetch songs data from pg")?;
+    Ok(Json(songs))
 }
