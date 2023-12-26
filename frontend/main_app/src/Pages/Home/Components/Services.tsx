5151443512f6f689b3abfbc7f43718f10733e126
olegus199 <okremnyov@yandex.ru>
2023-12-26 09:16:15 +0300

    fully responsive services section

--- frontend/main_app/src/Pages/Home/Components/Services.tsx
+++ frontend/main_app/src/Pages/Home/Components/Services.tsx
@@ -1,6 +1,5 @@
 import styles from "./Services.module.scss";
 import { FC } from "react";
-import { FaLongArrowAltRight } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import songwriting from "../../../assets/svg/songwriting.svg";
 import mixing from "../../../assets/svg/mixing.svg";
@@ -15,118 +14,113 @@ const Services: FC = () => {
         <h2 className={styles.h2}>Или закажите необходимую услугу</h2>
         <div className={styles.services_container}>
           <div className={styles.service}>
-            <img
-              src={songwriting}
-              alt="service songwriting"
-              className={styles.service_icon}
-            />
+            <div className={styles.icon_wrapper}>
+              <img
+                src={songwriting}
+                alt="service songwriting"
+                className={styles.service_icon}
+              />
+            </div>
             <div className={styles.service_text}>
               <h3 className={styles.h3}>Создание песни</h3>
               <p className={styles.description}>
                 Здесь должно быть краткое описание сервиса, его суть и смысл в
                 одном - двух предложениях
               </p>
-              <div className={styles.link_container}>
-                <NavLink
-                  to="services"
-                  className={styles.link}
-                >
-                  найти профессионала
-                </NavLink>
-                <FaLongArrowAltRight className={styles.arrow_icon} />
-              </div>
+              <NavLink
+                to="services"
+                className={styles.link}
+              >
+                найти профессионала
+              </NavLink>
             </div>
           </div>
           <div className={styles.service}>
-            <img
-              src={mixing}
-              alt="service songwriting"
-              className={styles.service_icon}
-            />
+            <div className={styles.icon_wrapper}>
+              <img
+                src={mixing}
+                alt="service mixing"
+                className={styles.service_icon}
+              />
+            </div>
             <div className={styles.service_text}>
               <h3 className={styles.h3}>Сведение / Мастеринг</h3>
               <p className={styles.description}>
                 Здесь должно быть краткое описание сервиса, его суть и смысл в
                 одном - двух предложениях
               </p>
-              <div className={styles.link_container}>
-                <NavLink
-                  to="services"
-                  className={styles.link}
-                >
-                  найти профессионала
-                </NavLink>
-                <FaLongArrowAltRight className={styles.arrow_icon} />
-              </div>
+              <NavLink
+                to="services"
+                className={styles.link}
+              >
+                найти профессионала
+              </NavLink>
             </div>
           </div>
           <div className={styles.service}>
-            <img
-              src={beatmaking}
-              alt="service beatmaking"
-              className={styles.service_icon}
-            />
+            <div className={styles.icon_wrapper}>
+              <img
+                src={beatmaking}
+                alt="service beatmaking"
+                className={styles.service_icon}
+              />
+            </div>
             <div className={styles.service_text}>
               <h3 className={styles.h3}>Написание бита</h3>
               <p className={styles.description}>
                 Здесь должно быть краткое описание сервиса, его суть и смысл в
                 одном - двух предложениях
               </p>
-              <div className={styles.link_container}>
-                <NavLink
-                  to="services"
-                  className={styles.link}
-                >
-                  найти профессионала
-                </NavLink>
-                <FaLongArrowAltRight className={styles.arrow_icon} />
-              </div>
+              <NavLink
+                to="services"
+                className={styles.link}
+              >
+                найти профессионала
+              </NavLink>
             </div>
           </div>
           <div className={styles.service}>
-            <img
-              src={ghostwriting}
-              alt="service ghostriting"
-              className={styles.service_icon}
-            />
+            <div className={styles.icon_wrapper}>
+              <img
+                src={ghostwriting}
+                alt="service ghostwriting"
+                className={styles.service_icon}
+              />
+            </div>
             <div className={styles.service_text}>
               <h3 className={styles.h3}>Гострайтинг</h3>
               <p className={styles.description}>
                 Здесь должно быть краткое описание сервиса, его суть и смысл в
                 одном - двух предложениях
               </p>
-              <div className={styles.link_container}>
-                <NavLink
-                  to="services"
-                  className={styles.link}
-                >
-                  найти профессионала
-                </NavLink>
-                <FaLongArrowAltRight className={styles.arrow_icon} />
-              </div>
+              <NavLink
+                to="services"
+                className={styles.link}
+              >
+                найти профессионала
+              </NavLink>
             </div>
           </div>
           <div className={styles.service}>
-            <img
-              src={cover_design}
-              alt="service cover designk"
-              className={styles.service_icon}
-            />
+            <div className={styles.icon_wrapper}>
+              <img
+                src={cover_design}
+                alt="service cover design"
+                className={styles.service_icon}
+              />
+            </div>
             <div className={styles.service_text}>
               <h3 className={styles.h3}>Дизайн обложки</h3>
               <p className={styles.description}>
                 Здесь должно быть краткое описание сервиса, его суть и смысл в
                 одном - двух предложениях
               </p>
-              <div className={styles.link_container}>
-                <NavLink
-                  to="services"
-                  className={styles.link}
-                >
-                  найти профессионала
-                </NavLink>
-                <FaLongArrowAltRight className={styles.arrow_icon} />
-              </div>
+              <NavLink
+                to="services"
+                className={styles.link}
+              >
+                найти профессионала
+              </NavLink>
             </div>
           </div>
         </div>
