fd71e7b07dab4af337d7190c33821cdffecddd41
ghashy <obsidian.musicwork@gmail.com>
2023-12-31 00:30:05 +0300

    add one more integration test

--- tests/api/upload_song.rs
+++ tests/api/upload_song.rs
@@ -1,18 +1,10 @@
 //! tests/api/upload_song.rs
 
-use std::collections::{HashMap, HashSet};
-
 use crate::helpers::{TestApp, TestUser};
-use mustore::{
-    config::Settings,
-    cornucopia::queries::{tests, user_auth_queries},
-    domain::requests::SubmitSongRequest,
-    service_providers::object_storage::presigned_post_form::PresignedPostData,
-};
-use reqwest::multipart::{Form, Part};
+use mustore::{config::Settings, domain::requests::SubmitSongRequest};
 
 #[tokio::test]
-async fn song_uploading() {
+async fn song_uploading_success() {
     let app = TestApp::spawn_app(Settings::load_configuration().unwrap()).await;
 
     let test_user = TestUser::generate_user(String::from("creator"));
@@ -67,4 +59,43 @@ async fn song_uploading() {
         .await
         .unwrap();
     assert_eq!(response.status().as_u16(), 201);
+}
+
+#[tokio::test]
+async fn song_uploading_without_files_fails() {
+    let app = TestApp::spawn_app(Settings::load_configuration().unwrap()).await;
+
+    let test_user = TestUser::generate_user(String::from("creator"));
+    app.register_user(&test_user, 1).await;
+    let client = reqwest::Client::builder()
+        .cookie_store(true)
+        .build()
+        .unwrap();
+    assert_eq!(app.login_user(&test_user, &client).await.as_u16(), 200);
+
+    let body = SubmitSongRequest {
+        song_master_object_key: "some_keyyyyyyyyyyyy".to_string(),
+        song_master_tagged_object_key: None,
+        song_multitrack_object_key: "some_keyyyyyyyyyyyy".to_string(),
+        song_cover_object_key: "some_keyyyyyyyyyyyy".to_string(),
+        name: "some_song".to_string(),
+        description: None,
+        tags: vec!["calm".to_string()],
+        primary_genre: "pop".to_string(),
+        secondary_genre: None,
+        tempo: 100,
+        duration: 30,
+        lyric: "this is song's lyric. Is it long enough or not?".to_string(),
+        price: 100.into(),
+        sex: mustore::domain::music_parameters::Sex::Female,
+        key: mustore::domain::music_parameters::MusicKey::a_major,
+    };
+
+    let response = client
+        .post(format!("{}/api/protected/creator/submit_song", app.address))
+        .json(&body)
+        .send()
+        .await
+        .unwrap();
+    assert_eq!(response.status().as_u16(), 417);
 }
