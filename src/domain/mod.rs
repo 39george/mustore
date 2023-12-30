b3a80e1ca56d1fb1f1c19f023a64052e96072e34
ghashy <obsidian.musicwork@gmail.com>
2023-12-30 10:54:38 +0300

    add common functions for validation

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,8 +1,38 @@
 //! src/domain/mod.rs
 
+use validator::ValidationError;
+
 pub mod music_parameters;
+pub mod queries;
+pub mod signup_token;
 pub mod user_candidate;
 pub mod user_email;
 pub mod user_name;
 pub mod user_password;
 pub mod user_role;
+
+pub fn forbidden_characters(input: &str) -> Result<(), ValidationError> {
+    let forbidden_characters =
+        ['/', '(', ')', '"', '<', '>', '\\', '{', '}', ';', ':'];
+
+    if input.chars().any(|g| forbidden_characters.contains(&g)) {
+        Err(ValidationError::new("String contains forbidden chars"))
+    } else {
+        Ok(())
+    }
+}
+
+pub fn validate_slice_bounds_characters(
+    values: &[String],
+    (min, max): (usize, usize),
+) -> Result<(), ValidationError> {
+    for val in values.iter() {
+        crate::domain::forbidden_characters(val)?;
+        if val.len() < min {
+            return Err(ValidationError::new("Value is too short"));
+        } else if val.len() > max {
+            return Err(ValidationError::new("Value is too long"));
+        }
+    }
+    Ok(())
+}
