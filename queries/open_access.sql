1cd28b5b453c29fe21ab29e82e1ae315450e5507
ghashy <obsidian.musicwork@gmail.com>
2024-01-01 20:43:40 +0300

    rename tags to moods

--- queries/open_access.sql
+++ queries/open_access.sql
@@ -22,10 +22,10 @@ UNION ALL
 --! get_genres_list
 SELECT name from genres;
 
---! get_tags_list
-SELECT name from tags;
+--! get_moods_list
+SELECT name from moods;
 
---! get_songs (sex?, tempo?, key?, genre?, tag?) : (cover_url?)
+--! get_songs (sex?, tempo?, key?, genre?, mood?) : (cover_url?)
 SELECT 
 song_id,
 created_at,
@@ -43,7 +43,7 @@ WHERE
     AND s.tempo BETWEEN ((:tempo)::smallint[])[1] AND ((:tempo)::smallint[])[2])
 AND ((:key)::musickey[] IS NULL OR s.key= ANY((:key)::musickey[]))
 AND ((:genre)::text[] IS NULL OR s.primary_genre::text = ANY((:genre)::text[]))
-AND ((:tag)::text[] IS NULL OR s.vibes::text[] && (:tag)::text[])
+AND ((:mood)::text[] IS NULL OR s.vibes::text[] && (:mood)::text[])
 ORDER BY
     CASE WHEN :sort_by = 'top_wished' THEN likes END DESC NULLS LAST,
     CASE WHEN :sort_by = 'top_listened' THEN listenings END DESC NULLS LAST,
