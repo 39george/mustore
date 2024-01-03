0be937252eff7273198ac485a41cda87776d162f
ghashy <obsidian.musicwork@gmail.com>
2024-01-03 22:10:45 +0300

    implement new api endpoints

--- src/routes/protected/user.rs
+++ src/routes/protected/user.rs
@@ -8,9 +8,7 @@ use axum::Json;
 use axum::Router;
 use axum_login::permission_required;
 use fred::clients::RedisPool;
-use fred::interfaces::HashesInterface;
 use fred::interfaces::KeysInterface;
-use fred::interfaces::TransactionInterface;
 use fred::prelude::RedisResult;
 use fred::types::Scanner;
 use futures::TryStreamExt;
@@ -18,13 +16,17 @@ use http::StatusCode;
 use mediatype::media_type;
 use mediatype::MediaTypeBuf;
 use time::OffsetDateTime;
-use tower_http::trace::TraceLayer;
 use validator::Validate;
 
 // ───── Current Crate Imports ────────────────────────────────────────────── //
 
 use crate::auth::users::AuthSession;
-use crate::domain::requests::UploadFileRequest;
+use crate::cornucopia::queries::user_access;
+use crate::cornucopia::queries::user_access::GetConversationsEntries;
+use crate::domain::requests::user_access::CreateConversationRequest;
+use crate::domain::requests::user_access::GetConversationRequest;
+use crate::domain::requests::user_access::SendMessageRequest;
+use crate::domain::requests::user_access::UploadFileRequest;
 use crate::routes::ResponseError;
 use crate::service_providers::object_storage::presigned_post_form::PresignedPostData;
 use crate::startup::AppState;
@@ -55,7 +57,10 @@ lazy_static::lazy_static! {
 pub fn user_router() -> Router<AppState> {
     Router::new()
         .route("/health_check", routing::get(health_check))
-        .route("/upload", routing::get(request_obj_storage_upload))
+        .route("/req_upload_form", routing::get(request_obj_storage_upload))
+        .route("/conversations", routing::get(get_conversations))
+        .route("/conversation_id", routing::get(get_conversation_id))
+        .route("/send_message", routing::post(send_message))
         .layer(permission_required!(crate::auth::users::Backend, "user"))
 }
 
@@ -112,6 +117,137 @@ async fn request_obj_storage_upload(
     Ok(Json(presigned_post_data))
 }
 
+#[tracing::instrument(name = "Get ordinary conversations list", skip_all)]
+async fn get_conversations(
+    auth_session: AuthSession,
+    State(app_state): State<AppState>,
+) -> Result<Json<Vec<GetConversationsEntries>>, ResponseError> {
+    let user = auth_session.user.ok_or(ResponseError::UnauthorizedError(
+        anyhow::anyhow!("No such user in AuthSession!"),
+    ))?;
+
+    let db_client = app_state
+        .pg_pool
+        .get()
+        .await
+        .context("Failed to get connection from postgres pool")?;
+
+    let entries = user_access::get_conversations_entries()
+        .bind(&db_client, &user.id)
+        .all()
+        .await
+        .context("Failed to get conversations list from pg")?;
+
+    Ok(Json(entries))
+}
+
+#[tracing::instrument(name = "Get conversation id by user id", skip_all)]
+async fn get_conversation_id(
+    auth_session: AuthSession,
+    State(app_state): State<AppState>,
+    Query(GetConversationRequest { with_user_id }): Query<
+        GetConversationRequest,
+    >,
+) -> Result<Json<Option<i32>>, ResponseError> {
+    let user = auth_session.user.ok_or(ResponseError::UnauthorizedError(
+        anyhow::anyhow!("No such user in AuthSession!"),
+    ))?;
+
+    let db_client = app_state
+        .pg_pool
+        .get()
+        .await
+        .context("Failed to get connection from postgres pool")?;
+
+    let conversation_id = user_access::get_conversation_by_user_id()
+        .bind(&db_client, &user.id, &with_user_id)
+        .one()
+        .await
+        .context("Failed to get conversation id by user id")?;
+
+    Ok(Json(conversation_id))
+}
+
+#[tracing::instrument(name = "Create new conversation", skip_all)]
+async fn create_new_conversation(
+    auth_session: AuthSession,
+    State(app_state): State<AppState>,
+    Query(CreateConversationRequest { with_user_id }): Query<
+        CreateConversationRequest,
+    >,
+) -> Result<Json<i32>, ResponseError> {
+    let user = auth_session.user.ok_or(ResponseError::UnauthorizedError(
+        anyhow::anyhow!("No such user in AuthSession!"),
+    ))?;
+
+    let mut db_client = app_state
+        .pg_pool
+        .get()
+        .await
+        .context("Failed to get connection from postgres pool")?;
+
+    let transaction = db_client
+        .transaction()
+        .await
+        .context("Failed to get a transaction from pg")?;
+
+    let conversation_id = user_access::create_new_conversation()
+        .bind(&transaction)
+        .one()
+        .await
+        .context("Failed to create new conversation")?;
+
+    let count = user_access::add_participants_to_conversation()
+        .bind(&transaction, &conversation_id, &user.id, &with_user_id)
+        .await
+        .context("Failed to add participants to the conversation")?;
+
+    transaction
+        .commit()
+        .await
+        .context("Failed to commit a pg transaction")?;
+
+    if count != 2 {
+        Err(ResponseError::InternalError(anyhow::anyhow!(
+            "Count was equal {count}, but should be 2"
+        )))
+    } else {
+        Ok(Json(conversation_id))
+    }
+}
+
+#[tracing::instrument(name = "Send a message", skip_all)]
+async fn send_message(
+    auth_session: AuthSession,
+    State(app_state): State<AppState>,
+    Json(params): Json<SendMessageRequest>,
+) -> Result<StatusCode, ResponseError> {
+    let user = auth_session.user.ok_or(ResponseError::UnauthorizedError(
+        anyhow::anyhow!("No such user in AuthSession!"),
+    ))?;
+
+    params.validate()?;
+
+    let db_client = app_state
+        .pg_pool
+        .get()
+        .await
+        .context("Failed to get connection from postgres pool")?;
+
+    user_access::insert_new_message()
+        .bind(
+            &db_client,
+            &params.conversation_id,
+            &params.service_id,
+            &user.id,
+            &params.reply_message_id,
+            &params.text,
+        )
+        .await
+        .context("Failed to add participants to the conversation")?;
+
+    Ok(StatusCode::CREATED)
+}
 // ───── Functions ────────────────────────────────────────────────────────── //
 
 #[tracing::instrument(
@@ -141,9 +277,9 @@ async fn check_current_user_uploads(
     let mut scan = con.next().scan(pattern, None, None);
     while let Ok(Some(mut page)) = scan.try_next().await {
         if let Some(keys) = page.take_results() {
-            if keys.len() > 10 {
+            if keys.len() > 15 {
                 tracing::error!(
-                    "User {} already have 10 current uploads",
+                    "User {} already have 15 current uploads",
                     user_id
                 );
                 return Err(ResponseError::TooManyUploadsError);
