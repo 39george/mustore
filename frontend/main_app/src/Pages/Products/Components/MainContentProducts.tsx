91e588ff38f0824059a04515a3943878eb2700a4
olegus199 <okremnyov@yandex.ru>
2024-01-08 09:34:26 +0300

    add error handling for genres request

--- frontend/main_app/src/Pages/Products/Components/MainContentProducts.tsx
+++ frontend/main_app/src/Pages/Products/Components/MainContentProducts.tsx
@@ -1,20 +1,84 @@
 import { IoSearch } from "react-icons/io5";
 import styles from "./MainContentProducts.module.scss";
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { GoChevronDown } from "react-icons/go";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { API_URL } from "../../../config";
 
-const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
+const MAX_RETRIES = 3;
+const RETRY_DELAY_MS = 400;
+
+type GenreOrMood = string[];
 
 const MainContentProducts: FC = () => {
+  const [genres, set_genres] = useState<GenreOrMood>([]);
+  const [genres_error, set_genres_error] = useState<string | null>(null);
+
+  // GET genres request
   useEffect(() => {
-    const get_genre_list = async () => {
+    const wait = (ms: number) =>
+      new Promise((resolve) => setTimeout(resolve, ms));
+
+    const get_genre_list = async (attempts: number = 1) => {
       try {
-        const response = await fetch(`${API_URL}/open/genres`);
-        const data = await response.json();
-        console.log(data);
+        const response = await axios.get<GenreOrMood>(`${API_URL}/open/genres`);
+        if (Array.isArray(response.data)) {
+          set_genres(response.data);
+        } else {
+          console.error("Unexpected response type:", response.data);
+        }
       } catch (error) {
-        console.log("error message:", error);
+        if (axios.isAxiosError(error)) {
+          if (error.response) {
+            console.error(
+              "API Error:",
+              error.response.status,
+              error.response.data
+            );
+
+            if (attempts < MAX_RETRIES) {
+              await wait(RETRY_DELAY_MS);
+              get_genre_list(attempts + 1);
+            } else {
+              handle_axios_error(error);
+            }
+          } else if (error.request) {
+            if (attempts < MAX_RETRIES) {
+              await wait(RETRY_DELAY_MS);
+              get_genre_list(attempts + 1);
+            } else {
+              set_genres_error(
+                "No response from server. Please, check your internet connection and try again"
+              );
+            }
+          } else {
+            set_genres_error("Error in setting up the request.");
+            console.error("API Error: Reqest setup error:", error.message);
+          }
+        } else {
+          set_genres_error("An unexpected error occured.");
+          console.error("Non-Axios:", error);
+        }
+      }
+    };
+
+    const handle_axios_error = (error: AxiosError) => {
+      if (error.response) {
+        switch (error.response.status) {
+          case 400:
+            set_genres_error(
+              "Bad input, please check your request and try again"
+            );
+            break;
+          case 500:
+            set_genres_error("Internal server error. Please try again later.");
+            break;
+          default:
+            set_genres_error(
+              "An unexpected error occured. Please, try again later."
+            );
+            break;
+        }
       }
     };
 
