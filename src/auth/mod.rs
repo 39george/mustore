45886d81180cd472af80e0f30d8c0ecac2f5ab66
ghashy <obsidian.musicwork@gmail.com>
2024-01-22 15:27:44 +0300

    refactor response types

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,5 +1,6 @@
 //! src/auth/mod.rs
 
+use axum::body::Body;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::response::Response;
@@ -19,6 +20,8 @@ pub mod users;
 
 #[derive(thiserror::Error)]
 pub enum AuthError {
+    #[error("Failed to parse credentials: {0}")]
+    ValidationError(#[source] anyhow::Error),
     #[error("Invalid credentials: {0}")]
     InvalidCredentialsError(#[source] anyhow::Error),
     #[error(transparent)]
@@ -41,6 +44,10 @@ impl IntoResponse for AuthError {
     fn into_response(self) -> Response {
         tracing::error!("{:?}", self);
         match self {
+            AuthError::ValidationError(e) => Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from(e.to_string()))
+                .unwrap_or(StatusCode::BAD_REQUEST.into_response()),
             AuthError::UnexpectedError(_) => {
                 StatusCode::INTERNAL_SERVER_ERROR.into_response()
             }
@@ -49,8 +56,9 @@ impl IntoResponse for AuthError {
             }
             AuthError::InvalidCredentialsError(_) => Response::builder()
                 .status(StatusCode::UNAUTHORIZED)
-                .body("Json auth is required".into())
-                .unwrap(),
+                .header("Content-Type", "application/json")
+                .body(Body::from("{\"caused_by\": \"Auth is required\"}"))
+                .unwrap_or(StatusCode::UNAUTHORIZED.into_response()),
             AuthError::SignupFailed(_) => Response::builder()
                 .status(StatusCode::BAD_REQUEST)
                 .body("Failed to signup".into())
