7369ddbe8f510dbf4ddc92c76d1e1f54de03ef8a
ghashy <obsidian.musicwork@gmail.com>
2024-01-22 17:02:17 +0300

    add new types to openapi type

--- src/startup/api_doc.rs
+++ src/startup/api_doc.rs
@@ -1,23 +1,55 @@
 use utoipa::{
-    openapi::security::{ApiKey, ApiKeyValue, SecurityScheme},
+    openapi::{
+        security::{ApiKey, ApiKeyValue, SecurityScheme},
+        ServerBuilder,
+    },
     Modify, OpenApi,
 };
 
+use crate::auth::{self, users::Permission};
 use crate::routes::open;
+use crate::routes::protected;
 
-// #[derive(utoipa::ToSchema)]
-// #[schema(as = anyhow::Error)]
-// struct AnyhowError {
-//     message: String,
-// }
+// ───── ErrorResponses ───────────────────────────────────────────────────── //
 
-// #[derive(utoipa::ToSchema)]
-// #[schema(as = garde::Report)]
-// struct GardeReport {
-//     message: String,
-// }
+#[derive(utoipa::ToResponse)]
+#[response(description = "Something happened on the server")]
+pub struct InternalErrorResponse;
 
-#[derive(utoipa::ToSchema)]
+#[derive(utoipa::ToResponse)]
+#[response(
+    description = "Request was formed erroneously",
+    content_type = "application/json",
+    example = json!({
+        "caused_by":
+        "Here will be the reason of a rejection"
+    }),
+)]
+pub struct BadRequestResponse(String);
+
+#[derive(utoipa::ToResponse)]
+#[response(description = "Not acceptable error")]
+pub struct NotAcceptableErrorResponse;
+
+#[derive(utoipa::ToResponse)]
+#[response(
+    description = "Unauthorized error",
+    content_type = "text/plain",
+    example = json!({
+        "caused_by":
+        "Auth is required"
+    }),
+)]
+pub struct UnauthorizedErrorResponse(String);
+
+#[derive(utoipa::ToResponse)]
+#[response(description = "Too many uploads error")]
+pub struct TooManyUploadsErrorResponse;
+
+// ───── Responses ────────────────────────────────────────────────────────── //
+
+#[derive(utoipa::ToSchema, utoipa::ToResponse)]
+#[response(description = "Person response returns single Person entity")]
 #[schema(as = GetSongsListResponse)]
 pub struct GetSongsListResponse {
     pub song_id: i32,
@@ -32,28 +64,141 @@ pub struct GetSongsListResponse {
     pub is_user_liked: Option<bool>,
 }
 
+#[derive(utoipa::ToSchema)]
+#[schema(as = GetNewSongsResponse)]
+pub struct GetNewSongsResponse {
+    pub song_id: i32,
+    pub created_at: time::OffsetDateTime,
+    pub cover_url: String,
+    pub name: String,
+    pub author: String,
+    pub likes: i64,
+    pub price: rust_decimal::Decimal,
+    pub is_user_liked: Option<bool>,
+}
+
+#[derive(utoipa::ToSchema)]
+#[schema(as = GetRecommendedSongsResponse)]
+pub struct GetRecommendedSongsResponse {
+    pub song_id: i32,
+    pub created_at: time::OffsetDateTime,
+    pub cover_url: String,
+    pub name: String,
+    pub author: String,
+    pub likes: i64,
+    pub price: rust_decimal::Decimal,
+    pub is_user_liked: Option<bool>,
+}
+
+#[derive(utoipa::ToSchema, utoipa::ToResponse)]
+#[response(
+    description = "If value exists",
+    content_type = "application/json",
+    example = json!({"exists": true }),
+)]
+pub struct IsExistsResponse(String);
+
+// ───── TypeWrappers ─────────────────────────────────────────────────────── //
+
+#[derive(utoipa::ToSchema)]
+#[schema(as = Secret)]
+pub struct Password(String);
+
+#[derive(utoipa::ToSchema)]
+#[schema(as = mediatype::MediaTypeBuf)]
+pub struct MediaType(String);
+
+// ───── Addons ───────────────────────────────────────────────────────────── //
+
+struct SecurityAddon;
+
+impl Modify for SecurityAddon {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        if let Some(components) = openapi.components.as_mut() {
+            components.add_security_scheme(
+                "api_key",
+                SecurityScheme::ApiKey(ApiKey::Cookie(ApiKeyValue::new("id"))),
+            )
+        }
+    }
+}
+
+struct ServerAddon;
+
+impl Modify for ServerAddon {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        let server = ServerBuilder::new()
+            .description(Some("Development server"))
+            .build();
+        openapi.servers = Some(vec![server]);
+    }
+}
+
+// ───── Api ──────────────────────────────────────────────────────────────── //
+
 #[derive(OpenApi)]
 #[openapi(
         paths(
         open::stats,
         open::get_values_list,
         open::get_songs,
+        open::get_new_songs,
+        open::get_recommended_songs,
+        auth::signup::signup,
+        auth::login::post::login,
+        auth::login::get::logout,
+        auth::login::get::username_status,
+        protected::health_check,
+        protected::user::health_check,
+        protected::creator::health_check,
+        protected::consumer::health_check,
+        protected::admin::health_check,
+        protected::user::user_permissions,
+        protected::user::request_obj_storage_upload
         ),
         components(
             schemas(
-                crate::routes::open::Stats,
+                crate::domain::requests::open_access::Stats,
                 crate::domain::requests::open_access::GetSongsListRequest,
+                crate::domain::requests::open_access::SongsAmount,
                 crate::domain::music_parameters::SortBy,
                 crate::domain::music_parameters::Sex,
                 crate::domain::music_parameters::MusicKey,
+                crate::domain::user_role::UserRole,
+                crate::auth::signup::UserSignupData,
+                crate::auth::login::Credentials,
+                crate::auth::login::Username,
                 GetSongsListResponse,
-                // crate::routes::ResponseError,
-                // AnyhowError,
-                // GardeReport
+                GetNewSongsResponse,
+                GetRecommendedSongsResponse,
+                Password,
+                crate::service_providers::object_storage::presigned_post_form::PresignedPostData,
+                MediaType
+            ),
+            responses(
+                InternalErrorResponse,
+                BadRequestResponse,
+                NotAcceptableErrorResponse,
+                UnauthorizedErrorResponse,
+                TooManyUploadsErrorResponse,
+                IsExistsResponse,
+                crate::service_providers::object_storage::presigned_post_form::PresignedPostData,
+                Permission
             )
         ),
+        modifiers(&ServerAddon),
         tags(
-            (name = "open", description = "Open routes (no authorization)")
+            (name = "open", description = "Open routes (no authorization)"),
+            (name = "health_checks", description = "Health checks for all access endpoints"),
+            (name = "protected.users", description = "Protected routes for all users"),
+            (name = "protected.creators", description = "Protected routes for creators"),
+            (name = "protected.consumers", description = "Protected routes for consumers"),
+            (name = "protected.admins", description = "Protected routes for admins")
+        ),
+        info(
+            title = "HarmonySphere - OpenAPI 3.0",
+            version = "0.1.0",
+            description = "This is a swagger documentation for harmonysphere backend application.",
         )
     )]
 pub(super) struct ApiDoc;
