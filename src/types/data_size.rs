4dc3584242b55863800ee0d47562fd0acbbc44a5
ghashy <obsidian.musicwork@gmail.com>
2024-01-23 01:58:50 +0300

    refactor datatypes fn names

--- src/types/data_size.rs
+++ src/types/data_size.rs
@@ -1,14 +1,24 @@
 pub trait DataSizes {
-    fn mb(self) -> Self;
-    fn gb(self) -> Self;
+    fn mb_to_bytes(self) -> Self;
+    fn gb_to_bytes(self) -> Self;
 }
 
-impl DataSizes for u64 {
-    fn mb(self) -> Self {
-        self * 1_000_000
-    }
+macro_rules! impl_datasizes {
+    ( $x:ty ) => {
+        impl DataSizes for $x {
+            fn mb_to_bytes(self) -> Self {
+                self * 1_000_000
+            }
 
-    fn gb(self) -> Self {
-        self * 1_000_000_000
-    }
+            fn gb_to_bytes(self) -> Self {
+                self * 1_000_000_000
+            }
+        }
+    };
 }
+
+impl_datasizes!(usize);
+impl_datasizes!(u64);
+impl_datasizes!(u32);
+impl_datasizes!(i64);
+impl_datasizes!(i32);
