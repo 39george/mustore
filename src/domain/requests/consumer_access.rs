ad8ea48efd08c7b8c420a74a4fdd8e8213662b15
ghashy <obsidian.musicwork@gmail.com>
2024-01-22 16:56:27 +0300

    remove unused imports

--- src/domain/requests/consumer_access.rs
+++ src/domain/requests/consumer_access.rs
@@ -1,12 +1,5 @@
-use garde::Validate;
-use rust_decimal::Decimal;
 use serde::Deserialize;
 use serde::Serialize;
-use time::OffsetDateTime;
-
-use crate::domain::music_parameters::MusicKey;
-use crate::domain::music_parameters::Sex;
-use crate::domain::*;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AcceptOffer {
