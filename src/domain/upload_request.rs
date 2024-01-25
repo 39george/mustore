8caa686ba9599523978e9e63dd82d7031301bd8c
ghashy <obsidian.musicwork@gmail.com>
2024-01-26 01:08:12 +0300

    improve error handling, integrate ObjectKey type, add unit test

--- src/domain/upload_request.rs
+++ src/domain/upload_request.rs
@@ -2,12 +2,16 @@ use std::{fmt::Display, num::ParseIntError, str::FromStr};
 
 use crate::error_chain_fmt;
 
+use super::object_key::{ObjectKey, ObjectKeyError};
+
 #[derive(thiserror::Error)]
 pub enum UploadRequestError {
     #[error(transparent)]
     ObjectStorageError(#[from] ParseIntError),
-    #[error("Some part of type not found in given str, can't parse into UploadRequest")]
-    PartNotFoundInStrError,
+    #[error("Some part of type not found in given str, can't parse into UploadRequest: {0}")]
+    PartNotFoundInStrError(String),
+    #[error(transparent)]
+    ObjectKeyError(#[from] ObjectKeyError),
 }
 
 impl std::fmt::Debug for UploadRequestError {
@@ -16,22 +20,23 @@ impl std::fmt::Debug for UploadRequestError {
     }
 }
 
+#[derive(Debug)]
 pub struct UploadRequest {
-    pub user_id: i32,
-    pub object_key: String,
+    user_id: i32,
+    object_key: ObjectKey,
 }
 
 impl UploadRequest {
-    pub fn new(user_id: i32, object_key: &str) -> Self {
+    pub fn new(user_id: i32, object_key: ObjectKey) -> Self {
         UploadRequest {
             user_id,
-            object_key: object_key.to_string(),
+            object_key,
         }
     }
-    pub fn get_user_id(&self) -> i32 {
+    pub fn user_id(&self) -> i32 {
         self.user_id
     }
-    pub fn get_object_key(&self) -> &str {
+    pub fn object_key(&self) -> &ObjectKey {
         &self.object_key
     }
 }
@@ -48,16 +53,32 @@ impl Display for UploadRequest {
 impl FromStr for UploadRequest {
     type Err = UploadRequestError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let elements = s.split(':').collect::<Vec<_>>();
+        let elements = s.splitn(3, ':').collect::<Vec<_>>();
         Ok(UploadRequest {
             user_id: elements
                 .get(1)
-                .ok_or(UploadRequestError::PartNotFoundInStrError)?
+                .ok_or(UploadRequestError::PartNotFoundInStrError(
+                    s.to_owned(),
+                ))?
                 .parse()?,
             object_key: elements
                 .get(2)
-                .ok_or(UploadRequestError::PartNotFoundInStrError)?
-                .to_string(),
+                .ok_or(UploadRequestError::PartNotFoundInStrError(
+                    s.to_owned(),
+                ))?
+                .parse()?,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn testme() {
+        let upload_req =
+            format!("upload_request:12:received/Carl:7FEE8A91-2620-4B9A-98E0-E7D13DA97A62:file.png").parse::<UploadRequest>();
+        assert!(upload_req.is_ok());
     }
 }
