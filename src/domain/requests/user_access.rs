69290401d6c65059db834f9d970e7c3bfcb25169
ghashy <obsidian.musicwork@gmail.com>
2024-01-26 01:04:41 +0300

    update openapi docs

--- src/domain/requests/user_access.rs
+++ src/domain/requests/user_access.rs
@@ -4,6 +4,7 @@ use serde::Serialize;
 use utoipa::IntoParams;
 use utoipa::ToSchema;
 
+use crate::domain::object_key::ObjectKey;
 use crate::domain::*;
 
 #[derive(Deserialize, Debug, Validate, ToSchema, IntoParams)]
@@ -15,33 +16,43 @@ pub struct UploadFileRequest {
         custom(forbidden_characters),
         custom(contains_no_control_characters)
     )]
-    #[schema(min_length = 2, max_length = 50, pattern = r#"[^/()"<>\\{};:]*"#)]
+    #[param(min_length = 2, max_length = 50, pattern = r#"[^/()"<>\\{};:]*"#)]
     pub file_name: String,
 }
 
+/// We don't use `ToSchema` here, because we specify it manually above handler
+/// in `params` section.
 #[derive(Deserialize, Debug)]
 pub struct GetConversationRequest {
-    pub with_user_id: i32,
+    pub with_user: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, ToSchema, IntoParams)]
 pub struct CreateConversationRequest {
-    pub with_user_id: i32,
+    #[param(example = "jack123")]
+    pub with_username: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, Validate)]
+#[derive(Serialize, Deserialize, Debug, Validate, ToSchema)]
 #[garde(allow_unvalidated)]
 pub struct SendMessageRequest {
+    /// Id of conversation
     pub conversation_id: i32,
+    /// Text of a message
+    #[schema(min_length = 1, max_length = 2500)]
     #[garde(length(min = MIN_MESSAGE_LEN, max = MAX_MESSAGE_LEN))]
     pub text: String,
+    /// Optional service id
     pub service_id: Option<i32>,
+    /// Optional attachments
+    #[schema(max_items = 10)]
     #[garde(length(min = 0, max = MAX_ATTACHMENTS_COUNT))]
-    pub attachments: Vec<String>,
+    pub attachments: Vec<ObjectKey>,
+    /// Optional reply message
     pub reply_message_id: Option<i32>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, IntoParams)]
 pub struct ListConversationRequest {
     pub conversation_id: i32,
     pub offset: i64,
