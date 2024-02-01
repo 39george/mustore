1c75df85bdda8ab5f2561ad663616d386c1ba1bb
olegus199 <okremnyov@yandex.ru>
2024-02-01 06:50:12 +0300

    delete unnecesary function

--- frontend/main_app/src/helpers/helpers.ts
+++ frontend/main_app/src/helpers/helpers.ts
@@ -1,30 +1,6 @@
-import { AxiosError } from "axios";
-
-// Error handling
+// Wait function
 export const wait = (ms: number) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
-};
-
-export const handle_axios_error = (
-  error: AxiosError,
-  set_error: (message: string) => void
-) => {
-  if (error.response) {
-    switch (error.response.status) {
-      case 400:
-        console.error("Bad request.", error.message);
-        break;
-      case 500:
-        set_error(
-          "Что-то не так с нашим сервером, мы уже работаем над этим. Пожалуйста, попробуйте обновить страницу"
-        );
-        console.error("Server responded with error: ", error.message);
-        break;
-      default:
-        console.error("An unexpected error occured: ", error.message);
-        break;
-    }
-  }
 };
 
 // Handling moving focus to the next input by pressing `enter` key
