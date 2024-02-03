013cba396d0d84ce7a3ffdf39a14b42883c19f52
olegus199 <okremnyov@yandex.ru>
2024-02-03 20:00:03 +0300

    complete logic and styles of usertoolbar

--- frontend/main_app/src/Components/UI/UserToolbar.tsx
+++ frontend/main_app/src/Components/UI/UserToolbar.tsx
@@ -2,52 +2,83 @@ import styles from "./UserToolbar.module.scss";
 import { FaRegHeart } from "react-icons/fa";
 import { PiShoppingCartFill } from "react-icons/pi";
 import avatar from "../../assets/HomePage/author_1.png";
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../state/store";
 import {
   ActiveSection,
   select_active_section,
 } from "../../state/active_section_slice";
+import ToolbarPopUpMenu from "./ToolbarPopUpMenu";
 
 const UserToolbar: FC = () => {
-  const [white_color, set_white_color] = useState("");
+  const [light_colors, set_light_colors] = useState("");
   const intersecting_section = useSelector<RootState, ActiveSection>((state) =>
     select_active_section(state.active_section)
   );
+  const [popup_visible, set_popup_visible] = useState(false);
+  const [styles_user_avatar, set_styles_user_avatar] = useState(
+    `${styles.wrapper}`
+  );
+  const user_avatar_container_ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     switch (intersecting_section) {
       case "hero":
-        set_white_color(``);
+        set_light_colors(``);
         break;
       case "why_us":
-        set_white_color(`${styles.white_icons}`);
+        set_light_colors(`${styles.light_colors}`);
         break;
       case "group":
-        set_white_color(``);
+        set_light_colors(``);
         break;
       case "authors_reviews":
-        set_white_color(`${styles.white_icons}`);
+        set_light_colors(`${styles.light_colors}`);
         break;
       case "footer":
-        set_white_color(``);
+        set_light_colors(``);
         break;
     }
   }, [intersecting_section]);
 
+  useEffect(() => {
+    if (popup_visible) {
+      set_styles_user_avatar(
+        `${styles.wrapper} ${styles.user_avatar_container_visible}`
+      );
+    } else {
+      set_styles_user_avatar(`${styles.wrapper}`);
+    }
+  }, [popup_visible]);
+
   return (
     <div className={styles.toolbar}>
-      <div className={styles.likes_container}>
-        <FaRegHeart className={`${styles.like_icon} ${white_color}`} />
-        <div className={styles.likes_amount}>2</div>
+      <div className={`${styles.wrapper} ${light_colors}`}>
+        <div className={styles.likes_container}>
+          <FaRegHeart className={styles.like_icon} />
+          <div className={styles.likes_amount}>2</div>
+        </div>
       </div>
-      <PiShoppingCartFill className={`${styles.cart_icon} ${white_color}`} />
-      <div className={styles.user_avatar_container}>
-        <img
-          src={avatar}
-          alt="user's avatar"
-          className={styles.user_avatar}
+      <div className={`${styles.wrapper} ${light_colors}`}>
+        <PiShoppingCartFill className={styles.cart_icon} />
+      </div>
+      <div
+        ref={user_avatar_container_ref}
+        className={`${styles_user_avatar} ${light_colors}`}
+        onClick={() => set_popup_visible(!popup_visible)}
+      >
+        <div className={styles.user_avatar_container}>
+          <img
+            src={avatar}
+            alt="user's avatar"
+            className={styles.user_avatar}
+          />
+        </div>
+        <ToolbarPopUpMenu
+          visible={popup_visible}
+          set_visible={set_popup_visible}
+          user_avatar_container_ref={user_avatar_container_ref.current}
         />
       </div>
     </div>
