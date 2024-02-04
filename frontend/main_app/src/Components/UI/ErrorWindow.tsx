4161db147ab1edab7977cb83c16b3b9fa84e1100
olegus199 <okremnyov@yandex.ru>
2024-02-04 07:32:00 +0300

    small changes

--- frontend/main_app/src/Components/UI/ErrorWindow.tsx
+++ frontend/main_app/src/Components/UI/ErrorWindow.tsx
@@ -2,6 +2,7 @@ import styles from "./ErrorWindow.module.scss";
 import { FC } from "react";
 import { FaCircleXmark } from "react-icons/fa6";
 import logo from "../../assets/svg/logo.svg";
+import { NavLink } from "react-router-dom";
 
 interface ErrorWindowProps {
   error_message: string;
@@ -15,11 +16,20 @@ const ErrorWindow: FC<ErrorWindowProps> = ({ error_message }) => {
           <p className={styles.general_info}>Что-то пошло не так...</p>
           <p className={styles.error_message}>{error_message}</p>
           <hr className={styles.divider} />
-          <img
-            src={logo}
-            alt="logo"
-            className={styles.logo}
-          />
+          <div className={styles.aux_info}>
+            <div className={styles.filler}></div>
+            <img
+              src={logo}
+              alt="logo"
+              className={styles.logo}
+            />
+            <NavLink
+              to="/"
+              className={styles.return_link}
+            >
+              На главную
+            </NavLink>
+          </div>
         </div>
       </div>
     </div>
