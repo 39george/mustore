9cc536735cf1d68d5064f373d0401ef582f41fa5
olegus199 <okremnyov@yandex.ru>
2024-02-18 21:17:18 +0300

    upload metainfo widget to a dashboard

--- frontend/main_app/src/PersonalAccount/Pages/Dashboard.tsx
+++ frontend/main_app/src/PersonalAccount/Pages/Dashboard.tsx
@@ -1,10 +1,11 @@
+import MetainfoWidget from "../Components/UI/MetaInfoWidget";
 import styles from "./Dashboard.module.scss";
 import { FC } from "react";
 
 const Dashboard: FC = () => {
   return (
-    <div>
-      <div>dashboard</div>
+    <div className={styles.dashboard}>
+      <MetainfoWidget />
     </div>
   );
 };
