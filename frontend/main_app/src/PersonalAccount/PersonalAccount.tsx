29200b2b9c1e93f60d754a5d4aa4d382ff6ec99b
olegus199 <okremnyov@yandex.ru>
2024-02-18 19:00:19 +0300

    small changes in personal account architecture

--- frontend/main_app/src/PersonalAccount/PersonalAccount.tsx
+++ frontend/main_app/src/PersonalAccount/PersonalAccount.tsx
@@ -3,13 +3,14 @@ import { Outlet } from "react-router-dom";
 import { FC } from "react";
 import Sidebar from "./Components/Sidebar";
 import TopBar from "./Components/TopBar";
+import avatar from "../assets/HomePage/author_1.png";
 
 const PersonalAccount: FC = () => {
   return (
     <div className={styles.personal_account}>
-      <Sidebar />
+      <Sidebar avatar={avatar} />
       <div className={styles.main_view}>
-        <TopBar />
+        <TopBar avatar={avatar} />
         <Outlet />
       </div>
     </div>
