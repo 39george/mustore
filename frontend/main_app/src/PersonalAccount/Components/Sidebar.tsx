3eeadfc1dbf9f669552a0c70dcb9c9b30f4ab539
olegus199 <okremnyov@yandex.ru>
2024-02-18 20:33:52 +0300

    add active logic and styles for sidebar

--- frontend/main_app/src/PersonalAccount/Components/Sidebar.tsx
+++ frontend/main_app/src/PersonalAccount/Components/Sidebar.tsx
@@ -1,8 +1,8 @@
 import styles from "./Sidebar.module.scss";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { IoChevronBackOutline } from "react-icons/io5";
 import { FaStar } from "react-icons/fa6";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import dashboard_icon from "../../assets/icons/dashboard.svg";
 import products_icon from "../../assets/icons/products.svg";
 import services_icon from "../../assets/icons/services.svg";
@@ -19,7 +19,61 @@ interface SidebarProps {
   avatar: string;
 }
 
+type ActiveSections =
+  | "dashboard"
+  | "products"
+  | "services"
+  | "conversations"
+  | "orders"
+  | "statistics"
+  | "earnings"
+  | "settings"
+  | "notifications"
+  | "help"
+  | "none";
+
 const Sidebar: FC<SidebarProps> = ({ avatar }) => {
+  const [active_section, set_active_section] = useState<ActiveSections>("none");
+  const location = useLocation();
+  const current_pathname = location.pathname.replace("/personal-account/", "");
+
+  useEffect(() => {
+    switch (current_pathname) {
+      case "dashboard":
+        set_active_section("dashboard");
+        break;
+      case "products":
+        set_active_section("products");
+        break;
+      case "services":
+        set_active_section("services");
+        break;
+      case "conversations":
+        set_active_section("conversations");
+        break;
+      case "orders":
+        set_active_section("orders");
+        break;
+      case "statistics":
+        set_active_section("statistics");
+        break;
+      case "earnings":
+        set_active_section("earnings");
+        break;
+      case "settings":
+        set_active_section("settings");
+        break;
+      case "notifications":
+        set_active_section("notifications");
+        break;
+      case "help":
+        set_active_section("help");
+        break;
+      default:
+        set_active_section("none");
+    }
+  }, [current_pathname]);
+
   return (
     <div className={styles.sidebar}>
       <div className={styles.collapse_icon_container}>
@@ -37,8 +91,8 @@ const Sidebar: FC<SidebarProps> = ({ avatar }) => {
           />
         </div>
         <div className={styles.meta_text}>
-          <p className={styles.username}>Alena NAI</p>
           <p className={styles.user_role}>Автор</p>
+          <p className={styles.username}>Alena NAI</p>
           <div className={styles.rating_container}>
             <FaStar className={styles.star_icon} />
             <p className={styles.rating}>
@@ -50,7 +104,9 @@ const Sidebar: FC<SidebarProps> = ({ avatar }) => {
       <div className={styles.tabs_container}>
         <NavLink
           to="dashboard"
-          className={styles.tab_link}
+          className={`${styles.tab_link} ${
+            active_section === "dashboard" && styles.tab_link_active
+          }`}
         >
           <img
             src={dashboard_icon}
@@ -62,7 +118,9 @@ const Sidebar: FC<SidebarProps> = ({ avatar }) => {
         <p className={styles.section_name}>Контент</p>
         <NavLink
           to="products"
-          className={styles.tab_link}
+          className={`${styles.tab_link} ${
+            active_section === "products" && styles.tab_link_active
+          }`}
         >
           <img
             src={products_icon}
@@ -73,7 +131,9 @@ const Sidebar: FC<SidebarProps> = ({ avatar }) => {
         </NavLink>
         <NavLink
           to="services"
-          className={styles.tab_link}
+          className={`${styles.tab_link} ${
+            active_section === "services" && styles.tab_link_active
+          }`}
         >
           <img
             src={services_icon}
@@ -85,7 +145,9 @@ const Sidebar: FC<SidebarProps> = ({ avatar }) => {
         <p className={styles.section_name}>Сотрудничесвто</p>
         <NavLink
           to="conversations"
-          className={styles.tab_link}
+          className={`${styles.tab_link} ${
+            active_section === "conversations" && styles.tab_link_active
+          }`}
         >
           <img
             src={conversations_icon}
@@ -96,7 +158,9 @@ const Sidebar: FC<SidebarProps> = ({ avatar }) => {
         </NavLink>
         <NavLink
           to="orders"
-          className={styles.tab_link}
+          className={`${styles.tab_link} ${
+            active_section === "orders" && styles.tab_link_active
+          }`}
         >
           <img
             src={orders_icon}
@@ -108,7 +172,9 @@ const Sidebar: FC<SidebarProps> = ({ avatar }) => {
         <p className={styles.section_name}>Эффективность</p>
         <NavLink
           to="statistics"
-          className={styles.tab_link}
+          className={`${styles.tab_link} ${
+            active_section === "statistics" && styles.tab_link_active
+          }`}
         >
           <img
             src={statistics_icon}
@@ -119,7 +185,9 @@ const Sidebar: FC<SidebarProps> = ({ avatar }) => {
         </NavLink>
         <NavLink
           to="earnings"
-          className={styles.tab_link}
+          className={`${styles.tab_link} ${
+            active_section === "earnings" && styles.tab_link_active
+          }`}
         >
           <img
             src={earnings_icon}
@@ -131,7 +199,9 @@ const Sidebar: FC<SidebarProps> = ({ avatar }) => {
         <p className={styles.section_name}>Аккаунт</p>
         <NavLink
           to="settings"
-          className={styles.tab_link}
+          className={`${styles.tab_link} ${
+            active_section === "settings" && styles.tab_link_active
+          }`}
         >
           <img
             src={settings_icon}
@@ -143,7 +213,9 @@ const Sidebar: FC<SidebarProps> = ({ avatar }) => {
         <p className={styles.section_name}>Общее</p>
         <NavLink
           to="notifications"
-          className={styles.tab_link}
+          className={`${styles.tab_link} ${
+            active_section === "notifications" && styles.tab_link_active
+          }`}
         >
           <img
             src={notifications_icon}
@@ -154,7 +226,9 @@ const Sidebar: FC<SidebarProps> = ({ avatar }) => {
         </NavLink>
         <NavLink
           to="help"
-          className={styles.tab_link}
+          className={`${styles.tab_link} ${
+            active_section === "help" && styles.tab_link_active
+          }`}
         >
           <img
             src={help_icon}
