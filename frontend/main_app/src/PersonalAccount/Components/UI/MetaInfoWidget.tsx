3f2b8541eb6793dcb28221b5da4d7e0c89b967e8
olegus199 <okremnyov@yandex.ru>
2024-02-18 21:16:32 +0300

    create widgets for dashboard

---
+++ frontend/main_app/src/PersonalAccount/Components/UI/MetaInfoWidget.tsx
@@ -0,0 +1,107 @@
+import styles from "./MetainfoWidget.module.scss";
+import { FC, useEffect, useState } from "react";
+import avatar from "../../../assets/HomePage/author_1.png";
+import { FaStar } from "react-icons/fa6";
+
+const month = new Date().getMonth();
+
+const MetainfoWidget: FC = () => {
+  const [current_month, set_current_month] = useState("");
+
+  useEffect(() => {
+    switch (month) {
+      case 0:
+        set_current_month("Январе");
+        break;
+      case 1:
+        set_current_month("Феврале");
+        break;
+      case 2:
+        set_current_month("Марте");
+        break;
+      case 3:
+        set_current_month("Апреле");
+        break;
+      case 4:
+        set_current_month("Мае");
+        break;
+      case 5:
+        set_current_month("Июне");
+        break;
+      case 6:
+        set_current_month("Июле");
+        break;
+      case 7:
+        set_current_month("Августе");
+        break;
+      case 8:
+        set_current_month("Сентябре");
+        break;
+      case 9:
+        set_current_month("Октябре");
+        break;
+      case 10:
+        set_current_month("Ноябре");
+        break;
+      case 11:
+        set_current_month("Декабре");
+        break;
+    }
+  }, [month]);
+
+  return (
+    <div className={styles.metainfo_widget}>
+      <div className={styles.header}>
+        <div className={styles.image_wrapper}>
+          <img
+            src={avatar}
+            alt="user's avatar"
+          />
+        </div>
+        <div className={styles.text_info}>
+          <div className={styles.name_and_role}>
+            <p className={styles.username}>Alena NAI</p>
+            <p className={styles.user_role}>Автор</p>
+          </div>
+          <div className={styles.rating_container}>
+            <FaStar className={styles.star_icon} />
+            <p className={styles.rating}>
+              5<span>(23)</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr className={styles.divider} />
+      <div className={styles.statistics}>
+        <div className={styles.statistics_unit}>
+          <p className={styles.statistics_text}>Ответы на новые сообщения</p>
+          <span className={styles.statistics_progress_circle}></span>
+          <p className={styles.statistics_result}>100%</p>
+        </div>
+        <div className={styles.statistics_unit}>
+          <p className={styles.statistics_text}>Время ответа на сообщения</p>
+          <p className={styles.statistics_result}>
+            1 <span>час</span>
+          </p>
+        </div>
+        <div className={styles.statistics_unit}>
+          <p className={styles.statistics_text}>Заказы, отданные в срок</p>
+          <span className={styles.statistics_progress_circle}></span>
+          <p className={styles.statistics_result}>75%</p>
+        </div>
+        <div className={styles.statistics_unit}>
+          <p className={styles.statistics_text}>Выполненные заказы</p>
+          <span className={styles.statistics_progress_circle}></span>
+          <p className={styles.statistics_result}>100%</p>
+        </div>
+      </div>
+      <hr className={styles.divider} />
+      <div className={styles.earnings}>
+        <p className={styles.earnings_text}>Заработанно в {current_month}</p>
+        <p className={styles.earnings_amount}>35 000 ₽</p>
+      </div>
+    </div>
+  );
+};
+
+export default MetainfoWidget;
