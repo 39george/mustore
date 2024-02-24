67afb79f8e8b61a24c04f7f0c64f64882c441666
olegus199 <okremnyov@yandex.ru>
2024-02-24 09:24:13 +0300

    begin dashboard responsive

--- frontend/main_app/src/PersonalAccount/Components/UI_Dashboard/MetaInfoWidget.tsx
+++ frontend/main_app/src/PersonalAccount/Components/UI_Dashboard/MetaInfoWidget.tsx
@@ -51,59 +51,63 @@ const MetainfoWidget: FC = () => {
 
   return (
     <div className={styles.metainfo_widget}>
-      <div className={styles.header}>
-        <div className={styles.image_wrapper}>
-          <img
-            src={avatar}
-            alt="user's avatar"
-          />
-        </div>
-        <div className={styles.text_info}>
-          <div className={styles.name_and_role}>
-            <p className={styles.username}>Alena NAI</p>
-            <p className={styles.user_role}>Автор</p>
+      <div className={styles.main_info}>
+        <div className={styles.header}>
+          <div className={styles.image_wrapper}>
+            <img
+              src={avatar}
+              alt="user's avatar"
+            />
           </div>
-          <div className={styles.rating_container}>
-            <FaStar className={styles.star_icon} />
-            <p className={styles.rating}>
-              5<span>(23)</span>
+          <div className={styles.text_info}>
+            <div className={styles.name_and_role}>
+              <p className={styles.username}>Alena NAI</p>
+              <p className={styles.user_role}>Автор</p>
+            </div>
+            <div className={styles.rating_container}>
+              <FaStar className={styles.star_icon} />
+              <p className={styles.rating}>
+                5<span>(23)</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <hr className={styles.divider} />
+        <div className={styles.statistics}>
+          <div className={styles.statistics_unit}>
+            <p className={styles.statistics_text}>Ответы на новые сообщения</p>
+            <div className={styles.statistics_progress}>
+              <span
+                className={`${styles.statistics_progress_circle} ${styles.circle_green}`}
+              ></span>
+              <p className={styles.statistics_result}>100%</p>
+            </div>
+          </div>
+          <div className={styles.statistics_unit}>
+            <p className={styles.statistics_text}>Время ответа на сообщения</p>
+            <p
+              className={`${styles.statistics_result} ${styles.not_percentage}`}
+            >
+              1 <span>час</span>
             </p>
           </div>
-        </div>
-      </div>
-      <hr className={styles.divider} />
-      <div className={styles.statistics}>
-        <div className={styles.statistics_unit}>
-          <p className={styles.statistics_text}>Ответы на новые сообщения</p>
-          <div className={styles.statistics_progress}>
-            <span
-              className={`${styles.statistics_progress_circle} ${styles.circle_green}`}
-            ></span>
-            <p className={styles.statistics_result}>100%</p>
+          <div className={styles.statistics_unit}>
+            <p className={styles.statistics_text}>Заказы, отданные в срок</p>
+            <div className={styles.statistics_progress}>
+              <span
+                className={`${styles.statistics_progress_circle} ${styles.circle_orange}`}
+              ></span>
+              <p className={styles.statistics_result}>75%</p>
+            </div>
           </div>
-        </div>
-        <div className={styles.statistics_unit}>
-          <p className={styles.statistics_text}>Время ответа на сообщения</p>
-          <p className={`${styles.statistics_result} ${styles.not_percentage}`}>
-            1 <span>час</span>
-          </p>
-        </div>
-        <div className={styles.statistics_unit}>
-          <p className={styles.statistics_text}>Заказы, отданные в срок</p>
-          <div className={styles.statistics_progress}>
-            <span
-              className={`${styles.statistics_progress_circle} ${styles.circle_orange}`}
-            ></span>
-            <p className={styles.statistics_result}>75%</p>
-          </div>
-        </div>
-        <div className={styles.statistics_unit}>
-          <p className={styles.statistics_text}>Выполненные заказы</p>
-          <div className={styles.statistics_progress}>
-            <span
-              className={`${styles.statistics_progress_circle} ${styles.circle_green}`}
-            ></span>
-            <p className={styles.statistics_result}>100%</p>
+          <div className={styles.statistics_unit}>
+            <p className={styles.statistics_text}>Выполненные заказы</p>
+            <div className={styles.statistics_progress}>
+              <span
+                className={`${styles.statistics_progress_circle} ${styles.circle_green}`}
+              ></span>
+              <p className={styles.statistics_result}>100%</p>
+            </div>
           </div>
         </div>
       </div>
