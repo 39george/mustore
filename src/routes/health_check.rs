fc59d96dcd0bfecf1fea1c739116793ef0fafe69
ghashy <obsidian.musicwork@gmail.com>
2024-02-25 21:53:32 +0300

    Format code

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -3,7 +3,9 @@ use std::net::SocketAddr;
 use axum::extract::ConnectInfo;
 use hyper::StatusCode;
 
-pub async fn health_check(ConnectInfo(my_connect_info): ConnectInfo<SocketAddr>) -> StatusCode {
+pub async fn health_check(
+    ConnectInfo(_my_connect_info): ConnectInfo<SocketAddr>,
+) -> StatusCode {
     // println!("Connect info: {}", my_connect_info);
     StatusCode::OK
 }
