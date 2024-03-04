ec53e15e6aea11944e92b9e77f19bcc0347eb85c
olegus199 <okremnyov@yandex.ru>
2024-03-04 08:48:21 +0300

    init AccountProducts and its components

---
+++ frontend/main_app/src/PersonalAccount/Components/UI_AccountProducts/ProductMeta.tsx
@@ -0,0 +1,125 @@
+import { MusicKey } from "../../../types/types";
+import styles from "./ProudctMeta.module.scss";
+import { FC } from "react";
+
+interface ProudctMetaProps {
+  likes_count: string;
+  listenings_count: string;
+  lyric: string;
+  moods: string[];
+  music_key: MusicKey;
+  name: string;
+  price: string;
+  primary_genre: string;
+  secondary_genre?: string;
+  sex: string;
+  song_id: string;
+  tempo: string;
+}
+
+const arr_to_string = (arr: string[]) => {
+  let result: string = "";
+  arr.forEach((string, idx) => {
+    result += string;
+    if (idx < arr.length - 1) {
+      result += ", ";
+    }
+  });
+  return result;
+};
+
+const format_price = (number_string: string) => {
+  return number_string.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+};
+
+const format_music_key = (music_key: string) => {
+  let formatted_music_key =
+    music_key.charAt(0).toUpperCase() +
+    music_key
+      .slice(1)
+      .replace("_sharp", "♯")
+      .replace("_flat", "♭")
+      .replace(/_/g, " ");
+
+  return formatted_music_key;
+};
+
+const format_sex = (sex: string) => {
+  let result = "";
+  switch (sex) {
+    case "male":
+      result = "Мужской";
+      break;
+    case "female":
+      result = "Женский";
+      break;
+  }
+
+  return result;
+};
+
+const ProductMeta: FC<ProudctMetaProps> = ({
+  likes_count,
+  listenings_count,
+  lyric,
+  moods,
+  music_key,
+  name,
+  price,
+  primary_genre,
+  secondary_genre,
+  sex,
+  tempo,
+}) => {
+  return (
+    <div className={styles.product_meta}>
+      <div className={styles.meta_header}>
+        <p className={styles.name}>{name}</p>
+        <p className={styles.edit}>редактировать</p>
+      </div>
+      <ul className={styles.meta_list}>
+        <li className={styles.meta_item}>
+          <p className={styles.meta_type}>Жанр</p>
+          <p className={styles.meta_value}>
+            {primary_genre}
+            {secondary_genre ? `, ${secondary_genre}` : ""}
+          </p>
+        </li>
+        <li className={styles.meta_item}>
+          <p className={styles.meta_type}>Mood</p>
+          <p className={styles.meta_value}>{arr_to_string(moods)}</p>
+        </li>
+        <li className={styles.meta_item}>
+          <p className={styles.meta_type}>Лайки</p>
+          <p className={styles.meta_value}>{likes_count}</p>
+        </li>
+        <li className={styles.meta_item}>
+          <p className={styles.meta_type}>Прослушивания</p>
+          <p className={styles.meta_value}>{listenings_count}</p>
+        </li>
+        <li className={styles.meta_item}>
+          <p className={styles.meta_type}>Цена</p>
+          <p className={styles.meta_value}>{format_price(price)}</p>
+        </li>
+        <li className={styles.meta_item}>
+          <p className={styles.meta_type}>Темп</p>
+          <p className={styles.meta_value}>{tempo} BPM</p>
+        </li>
+        <li className={styles.meta_item}>
+          <p className={styles.meta_type}>Тональность</p>
+          <p className={styles.meta_value}>{format_music_key(music_key)}</p>
+        </li>
+        <li className={styles.meta_item}>
+          <p className={styles.meta_type}>Вокал</p>
+          <p className={styles.meta_value}>{format_sex(sex)}</p>
+        </li>
+        <li className={styles.meta_item}>
+          <p className={styles.meta_type}>Текст</p>
+          <p className={styles.meta_value}>{lyric}</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ProductMeta;
---
+++ frontend/main_app/src/PersonalAccount/Components/UI_AccountProducts/ProductSection.tsx
@@ -0,0 +1,97 @@
+import styles from "./ProductSection.module.scss";
+import { FiPlus } from "react-icons/fi";
+import { FC, useState } from "react";
+import { GoChevronLeft, GoChevronRight } from "react-icons/go";
+import {
+  IProduct,
+  ProductSectionType,
+  TypeDeclension,
+} from "../../../types/types";
+import ProductMeta from "./ProductMeta";
+
+type NewDeclension = "новый" | "новую";
+
+interface ProductSectionProps {
+  type: ProductSectionType;
+  type_declension: TypeDeclension;
+  new_declension: NewDeclension;
+  products: IProduct[];
+}
+
+const ProductSection: FC<ProductSectionProps> = ({
+  type,
+  type_declension,
+  new_declension,
+  products,
+}) => {
+  const [product_idx, set_product_idx] = useState(0);
+  return (
+    <div className={styles.product_section}>
+      <div className={styles.product_header}>
+        <h2 className={styles.h2}>{type}</h2>
+        <div className={styles.upload_product}>
+          <p className={styles.upload_product_p}>
+            загрузить {new_declension} {type_declension}
+          </p>
+          <FiPlus className={styles.plus_icon} />
+        </div>
+      </div>
+      <div className={styles.product_content}>
+        <div className={styles.select_product}>
+          <div className={styles.covers_container}>
+            <div className={styles.nav_button}>
+              <GoChevronLeft className={styles.nav_chevron} />
+            </div>
+            {products.map((product, idx) => {
+              return (
+                <div
+                  className={`${styles.image_wrapper} ${styles.product}${
+                    idx + 1
+                  }`}
+                  key={idx}
+                >
+                  <img
+                    src={product.key}
+                    alt={`product${idx + 1}`}
+                  />
+                </div>
+              );
+            })}
+            <div className={styles.nav_button}>
+              <GoChevronRight className={styles.nav_chevron} />
+            </div>
+          </div>
+          <ol className={styles.product_list}>
+            {products.map((product, idx) => {
+              return (
+                <li
+                  className={styles.product_list_item}
+                  key={idx}
+                >
+                  <p className={styles.name}>{product.name}</p>
+                  <p className={styles.duration}>{product.duration}</p>
+                </li>
+              );
+            })}
+          </ol>
+        </div>
+        <ProductMeta
+          likes_count={products[product_idx].likes_count}
+          listenings_count={products[product_idx].listenings_count}
+          lyric={products[product_idx].lyric}
+          moods={products[product_idx].moods}
+          music_key={products[product_idx].music_key}
+          name={products[product_idx].name}
+          price={products[product_idx].price}
+          primary_genre={products[product_idx].primary_genre}
+          secondary_genre={products[product_idx].secondary_genre}
+          sex={products[product_idx].sex}
+          song_id={products[product_idx].song_id}
+          tempo={products[product_idx].tempo}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProductSection;
