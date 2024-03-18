fe63ab24fa68e68b42f9e4cdecd5238f415f9e01
olegus199 <okremnyov@yandex.ru>
2024-03-18 20:22:47 +0300

    create a link to UploadNewProducts component

--- frontend/main_app/src/PersonalAccount/Components/UI_AccountProducts/ProductDefault.tsx
+++ frontend/main_app/src/PersonalAccount/Components/UI_AccountProducts/ProductDefault.tsx
@@ -1,15 +1,26 @@
 import styles from "./ProductDefault.module.scss";
 import { FiPlus } from "react-icons/fi";
 import { FC } from "react";
+import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { set_product_status } from "../../../state/product_status_slice";
 
 const ProductDefault: FC = () => {
+  const dispatch = useDispatch();
+  const handle_click = () => {
+    dispatch(set_product_status(null));
+  };
   return (
     <div className={styles.product_default}>
       <h2 className={styles.h2}>Здесь пока ничего нет!</h2>
-      <div className={styles.upload_product}>
+      <NavLink
+        to="upload_new_product"
+        className={styles.upload_product}
+        onClick={handle_click}
+      >
         <p className={styles.upload_product_p}>загрузить новый товар</p>
         <FiPlus className={styles.plus_icon} />
-      </div>
+      </NavLink>
     </div>
   );
 };
