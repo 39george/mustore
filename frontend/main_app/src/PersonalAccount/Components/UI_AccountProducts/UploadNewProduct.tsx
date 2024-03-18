12f18a6d7a0019b623297f5680557292a6d37b27
olegus199 <okremnyov@yandex.ru>
2024-03-18 20:21:04 +0300

    init UploadNewProduct component

---
+++ frontend/main_app/src/PersonalAccount/Components/UI_AccountProducts/UploadNewProduct.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const UploadNewProduct: FC = () => {
+  return (
+    <div>
+      <div>upload new product</div>
+    </div>
+  );
+};
+
+export default UploadNewProduct;
