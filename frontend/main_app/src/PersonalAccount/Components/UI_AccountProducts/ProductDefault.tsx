f5b3e845e0aa6631bd6da135e73b3cdf6753b184
olegus199 <okremnyov@yandex.ru>
2024-03-21 20:30:07 +0300

    change nav link to link

--- frontend/main_app/src/PersonalAccount/Components/UI_AccountProducts/ProductDefault.tsx
+++ frontend/main_app/src/PersonalAccount/Components/UI_AccountProducts/ProductDefault.tsx
@@ -1,7 +1,7 @@
 import styles from "./ProductDefault.module.scss";
 import { FiPlus } from "react-icons/fi";
 import { FC } from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { set_product_status } from "../../../state/product_status_slice";
 
@@ -13,14 +13,14 @@ const ProductDefault: FC = () => {
   return (
     <div className={styles.product_default}>
       <h2 className={styles.h2}>Здесь пока ничего нет!</h2>
-      <NavLink
+      <Link
         to="upload_new_product"
         className={styles.upload_product}
         onClick={handle_click}
       >
         <p className={styles.upload_product_p}>загрузить новый товар</p>
         <FiPlus className={styles.plus_icon} />
-      </NavLink>
+      </Link>
     </div>
   );
 };
