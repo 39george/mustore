2012b6ce16fbf1d7e57ffd332b86cc79e392dd77
olegus199 <okremnyov@yandex.ru>
2024-04-22 07:51:02 +0300

    Add postcss processor

--- frontend/main_app/vite.config.ts
+++ frontend/main_app/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
   server: {
     port: 5173,
     strictPort: true,
